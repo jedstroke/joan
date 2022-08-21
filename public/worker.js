119b6ccf811942822ee3d3418d2fb3979ac13dfc
jedstroke <jedstroke@gmail.com>
2022-08-21 12:03:32 +0100

    Mid Milestone

---
+++ public/worker.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = "React_Joan_App_"
+
+var urlsToCache = [
+  'https://res.cloudinary.com/jedstroke/image/upload/v1661032883/IMG_20220612_103555_500_2_1_yhshji.jpg',
+  'https://res.cloudinary.com/jedstroke/video/upload/v1660959435/Hollow_Coves_-_These_Memories_tlcjqg.mp3',
+  'https://res.cloudinary.com/jedstroke/raw/upload/v1660959468/Mellow_cs0wrz.otf'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['your-app-name'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import './App.css';
-
 function App() {
+  const heart = useRef(null);
+  const poem = useRef(null);
+  const preloader = useRef(null);
+  const toast1 = () => {
+    toast('Wait for it 👏🏽', {
+      id:'toast one',
+      duration: 1500
+    });
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const intersecting = entry.isIntersecting
+        console.log(`${entry.target}: ${intersecting} `);
+      })
+    })
+    observer.observe(poem.current);
+    toast1();
+  }, [])
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={preloader} className='preloader'>
+        <div className='preloaderContent'>
+          <div>
+          <img className='heart' ref={heart} src="https://res.cloudinary.com/jedstroke/image/upload/v1661012008/heart_3_ihwvqu.png" alt="" />
+          </div>
+          <div>
+          <p>For Joan</p>
+          </div>
+        </div>
+      </div>
+      <div className='content'>
+      <div className='overlay'>
+      <div ref={poem} className='poem'>
+
+      </div>
+      </div>
+      <img src="https://res.cloudinary.com/jedstroke/image/upload/v1661032883/IMG_20220612_103555_500_2_1_yhshji.jpg" alt="" onLoad={() => {
+        heart.current.classList.add('heartZoom');
+        setTimeout(() => {
+          console.log('2');
+            preloader.current.classList.add('fadeOut');
+            setTimeout(() => {
+              console.log('3');
+              preloader.current.style.display = 'none';
+            }, 2000)
+        }, 5000)
+      }} />
+      </div>
     </div>
+    <Toaster toastOptions={{
+    style: {
+      fontFamily: 'Mellow',
+      color: '#424449',
+      fontWeight:'bold',
+      padding: '2px',
+      fontSize:'18px'
+    },
+  }}/>
+    </>
   );
 }
 
