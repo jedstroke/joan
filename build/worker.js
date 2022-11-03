e2568a78c07f1f8e7309d9c78d51c5f54e1707ea
jedstroke <jedstroke@gmail.com>
2022-11-03 05:26:19 -0700

    Optimised Last

--- build/worker.js
+++ build/worker.js
@@ -1,9 +1,11 @@
 var CACHE_NAME = "React_Joan_App_"
 
 var urlsToCache = [
+  './Futura-Bold.ttf',
+  './Coco-Light.tff',
+  'https://res.cloudinary.com/jedstroke/image/upload/v1661012008/heart_3_ihwvqu.png',
   'https://res.cloudinary.com/jedstroke/image/upload/v1661032883/IMG_20220612_103555_500_2_1_yhshji.jpg',
   'https://res.cloudinary.com/jedstroke/video/upload/v1660959435/Hollow_Coves_-_These_Memories_tlcjqg.mp3',
-  'https://res.cloudinary.com/jedstroke/raw/upload/v1660959468/Mellow_cs0wrz.otf'
 ];
 
 // Install a service worker
@@ -37,18 +39,3 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['React_Joan_App_'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
